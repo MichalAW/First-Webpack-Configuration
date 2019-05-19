596721f97178c91691fe8829b983f35656b11067
MichalAW <michal.wanta@gmail.com>
2019-05-19 14:57:43 +0200

    finished project

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+//run webpack.config.js
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    }
+};
